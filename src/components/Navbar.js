95a2d7b7997b34f7f68765c39fb5b8731cdae136
Kyle <kylev006@gmail.com>
2023-06-09 20:30:28 -0700

    added a NavBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Github, Linkedin } from "react-bootstrap-icons";
+
+function Topbar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#commits">Commits</Nav.Link>
+                        
+                    </Nav>
+                    <Nav>
+                        <Nav.Link className="px-3" href="https://www.linkedin.com/in/kyle-v-w/" target="_blank">
+                           <Linkedin /> 
+                        </Nav.Link>
+                        <Nav.Link href="https://github.com/Kwyk25" target="_blank">
+                            <Github />
+                        </Nav.Link> 
+                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Topbar;
